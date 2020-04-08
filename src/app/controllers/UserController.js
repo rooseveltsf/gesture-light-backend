416762496476424552e0cefa8f5d803961749e2f
RooseveltSouza <rooseveltsouzav12@gmail.com>
2020-04-08 15:14:20 -0300

    feat: add usercontroller

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,31 @@
+import * as Yup from 'yup';
+import User from '../models/User';
+
+class UserController {
+  async store(req, res, next) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      endereco: Yup.string().required(),
+      email: Yup.string().required(),
+      password: Yup.string().required().min(8),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Erro de validação' });
+    }
+
+    const userExists = await User.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (userExists) {
+      return res.status(400).json({ error: 'Usuário já existe' });
+    }
+
+    const { name, endereco, email } = await User.create(req.body);
+
+    return res.json({ name, endereco, email });
+  }
+}
+
+export default new UserController();
