a56c3ea0e8bc5e003e57aefc6c73224f2fc649a6
RooseveltSouza <rooseveltsouzav12@gmail.com>
2020-04-08 23:09:25 -0300

    test: add test de criptografia de senha

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -1,5 +1,8 @@
 import request from 'supertest';
+import bcrypt from 'bcryptjs';
+
 import app from '../../src/app';
+import User from '../../src/app/models/User';
 
 import truncate from '../util/truncate';
 
@@ -7,6 +10,19 @@ describe('User', () => {
   beforeEach(async () => {
     await truncate();
   });
+
+  it('deve ser possivel cryptografar a senha informada no cadastro', async () => {
+    const user = await User.create({
+      name: 'Roosevelt de Souza',
+      endereco: 'Praia de acaú',
+      email: 'roosevelt@mail.com',
+      password: 'llruy007',
+    });
+
+    const compareHash = await bcrypt.compare('llruy007', user.password_hash);
+    expect(compareHash).toBe(true);
+  });
+
   it('deve ser possivel se cadastrar', async () => {
     const response = await request(app).post('/users').send({
       name: 'Roosevelt de Souza',
@@ -28,6 +44,17 @@ describe('User', () => {
 
     const response = await request(app).post('/users').send({
       name: 'Roosevelt de Souza',
+      endereco: 'Praia de acaú',
+      email: 'roosevelt@mail.com',
+      password: 'llruy007',
+    });
+
+    expect(response.status).toBe(400);
+  });
+
+  it('não deve ser possivel se cadastrar faltando informações', async () => {
+    const response = await request(app).post('/users').send({
+      name: '',
       endereco: 'Praia de acaú',
       email: 'roosevelt@mail.com',
       password: 'llruy007',
