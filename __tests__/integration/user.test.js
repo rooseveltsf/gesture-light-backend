6212e0ed873b4b96c42fe893a941d331fcb7a9f7
RooseveltSouza <rooseveltsouzav12@gmail.com>
2020-04-08 22:50:01 -0300

    test: add teste de email duplicado

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -1,7 +1,12 @@
 import request from 'supertest';
 import app from '../../src/app';
 
+import truncate from '../util/truncate';
+
 describe('User', () => {
+  beforeEach(async () => {
+    await truncate();
+  });
   it('deve ser possivel se cadastrar', async () => {
     const response = await request(app).post('/users').send({
       name: 'Roosevelt de Souza',
@@ -11,5 +16,23 @@ describe('User', () => {
     });
 
     expect(response.body).toHaveProperty('name');
+  });
+
+  it('não deve ser possivel se cadastrar com email duplicado', async () => {
+    await request(app).post('/users').send({
+      name: 'Roosevelt de Souza',
+      endereco: 'Praia de acaú',
+      email: 'roosevelt@mail.com',
+      password: 'llruy007',
+    });
+
+    const response = await request(app).post('/users').send({
+      name: 'Roosevelt de Souza',
+      endereco: 'Praia de acaú',
+      email: 'roosevelt@mail.com',
+      password: 'llruy007',
+    });
+
+    expect(response.status).toBe(400);
   });
 });
