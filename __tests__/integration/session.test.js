ac51ceb93d4e7fa4e94489c52b9742dd66e59031
RooseveltSouza <rooseveltsouzav12@gmail.com>
2020-09-25 09:32:27 -0300

    test: testa se é possivel realizar login sem ter cadastro

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -3,4 +3,18 @@ import truncate from '../util/truncate';
 import factory from '../factories';
 import app from '../../src/app';
 
-describe('Session', () => {});
+describe('Session', () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
+  it('Não deve ser possível efetuar login sem ter cadastro', async () => {
+    const credentials = await factory.attrs('Session');
+
+    const response = await request(app).post('/session').send(credentials);
+
+    expect(response.status).toBe(400);
+  });
+
+  // it('Verificar ')
+});
