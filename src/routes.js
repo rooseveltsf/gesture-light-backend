d6fb962a460689361d54f4a1b37f6400e282928b
RooseveltSouza <rooseveltsouzav12@gmail.com>
2020-09-26 21:57:26 -0300

    chore: adiciona middleware de verificação de token

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,15 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import PublicationController from './app/controllers/PublicationController';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 
 routes.post('/session', SessionController.store);
+
+routes.use(authMiddleware);
 
 routes.get('/publish', PublicationController.index);
 
