e54842dc12522463468728080aadf917dc9a1719
RooseveltSouza <rooseveltsouzav12@gmail.com>
2020-09-26 21:00:05 -0300

    feat: cria model de imagens

---
+++ src/app/models/Image.js
@@ -0,0 +1,25 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Image extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        path: Sequelize.STRING,
+        url: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return `http://192.168.0.107:3333/publish/${this.path}`;
+          },
+        },
+      },
+      {
+        sequelize,
+      }
+    );
+
+    return this;
+  }
+}
+
+export default Image;
