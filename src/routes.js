e7c88d59666d99d77cf1192fd33c325d0130f751
RooseveltSouza <rooseveltsouzav12@gmail.com>
2020-09-26 21:09:40 -0300

    feat: add rota de listagem de publicação

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,14 @@ import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import PublicationController from './app/controllers/PublicationController';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 
 routes.post('/session', SessionController.store);
+
+routes.get('/publish', PublicationController.index);
 
 export default routes;
