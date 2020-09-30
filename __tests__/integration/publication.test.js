fc6ad3afe3b7dfc15ea11ce2b664d7d865cf6532
RooseveltSouza <rooseveltsouzav12@gmail.com>
2020-09-30 10:52:29 -0300

    test: testa rota de nova publicação

--- __tests__/integration/publication.test.js
+++ __tests__/integration/publication.test.js
@@ -1,34 +1,61 @@
 import request from 'supertest';
-import jwt from 'jsonwebtoken';
 
-import authConfig from '../../src/config/auth';
 import truncate from '../util/truncate';
 import factory from '../factories';
 import app from '../../src/app';
 
+let token = null;
+
 describe('Publications', () => {
   beforeEach(async () => {
     await truncate();
+
+    const user = await factory.create('User');
+
+    token = user.generateToken();
   });
 
-  it('Verifica se usuário existe antes da listagem de publicação', async () => {
-    const response = await request(app).get('/publish');
+  describe('GET /publish', () => {
+    it('Verifica se usuário existe antes da listagem de publicação', async () => {
+      const response = await request(app).get('/publish');
 
-    expect(response.status).toBe(401);
-  });
-
-  it('Verifica se está listando as publicações', async () => {
-    const { id } = await factory.create('User', {
-      email: 'roosevelt@test.com',
-      password: '12345678',
+      expect(response.status).toBe(401);
     });
 
-    const token = jwt.sign({ id }, authConfig.secret);
+    it('Verifica se está listando as publicações', async () => {
+      const response = await request(app)
+        .get('/publish')
+        .set('Authorization', `Bearer ${token}`);
 
-    const response = await request(app)
-      .get('/publish')
-      .set('Authorization', `Bearer ${token}`);
+      expect(response.status).toBe(200);
+    });
+  });
 
-    expect(response.status).toBe(200);
+  describe('POST /publish', () => {
+    it('Verifica se a publicação é feita', async () => {
+      const {
+        title,
+        description,
+        address,
+        status,
+        latitude,
+        longitude,
+      } = await factory.attrs('Publication');
+
+      const dir = `${__dirname}/../util/test.jpg`;
+
+      const response = await request(app)
+        .post('/publish')
+        .field('title', title)
+        .field('description', description)
+        .field('status', status)
+        .field('address', address)
+        .field('latitude', latitude)
+        .field('longitude', longitude)
+        .attach('file', dir)
+        .set('Authorization', `Bearer ${token}`);
+
+      expect(response.status).toBe(200);
+    });
   });
 });
