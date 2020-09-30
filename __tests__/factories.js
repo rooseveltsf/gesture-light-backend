53b2e4cc27df5d9457a1e4e9104ef40f97d4d6da
RooseveltSouza <rooseveltsouzav12@gmail.com>
2020-09-30 10:51:40 -0300

    feat: factory de publication

--- __tests__/factories.js
+++ __tests__/factories.js
@@ -1,6 +1,7 @@
 import faker from 'faker';
 import { factory } from 'factory-girl';
 import User from '../src/app/models/User';
+import Publication from '../src/app/models/Publication';
 
 factory.define('User', User, {
   name: faker.name.findName(),
@@ -9,9 +10,13 @@ factory.define('User', User, {
   password: faker.internet.password(),
 });
 
-factory.define('Session', User, {
-  email: faker.internet.email(),
-  password: faker.internet.password(),
+factory.define('Publication', Publication, {
+  title: faker.lorem.slug(8),
+  description: faker.lorem.paragraph(),
+  status: 'acessible',
+  address: faker.address.city(),
+  latitude: faker.address.latitude(),
+  longitude: faker.address.longitude(),
 });
 
 export default factory;
